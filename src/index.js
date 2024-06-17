de244932f680cf1bac4e598c54b62a1b63ef553e
HollowLoongG <128145349+HollowLoongG@users.noreply.github.com>
2024-06-17 14:59:58 +0800

    Update index.js
    customs

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.mirrors.jibril.us": dockerHub,
+  "quay.mirrors.jibril.us": "https://quay.io",
+  "gcr.mirrors.jibril.us": "https://gcr.io",
+  "k8s-gcr.mirrors.jibril.us": "https://k8s.gcr.io",
+  "k8s.mirrors.jibril.us": "https://registry.k8s.io",
+  "ghcr.mirrors.jibril.us": "https://ghcr.io",
+  "cloudsmith.mirrors.jibril.us": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mirrors.jibril.us": dockerHub,
 };
 
 function routeByHosts(host) {
